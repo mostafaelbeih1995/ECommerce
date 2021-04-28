adf14f691fe522d9b4f43b0b62000d24f953fdfe
Mostafa Elbeih <mte10@mail.aub.edu>
2021-04-28 14:25:01 +0200

    code for heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
+const path = require("path");
+
 require("dotenv").config();
 const app = express();
+app.use(express.static(path.resolve(__dirname, "../frontend", "build", "index.html")));
 app.use(express.json());
+app.use("/api/products", productRoutes);
 
-app.use("/api/products",productRoutes);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static("frontend/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+    });
+}
 
 connectDB();
 const PORT = process.env.PORT || 5000;
